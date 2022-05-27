5be98c7022a6a90578378d30ae531a74a2905b99
= <liam2robertson@gmail.com>
2022-05-27 14:51:06 +0100

    add tests for adding tasks

---
+++ tests/test_add.py
@@ -0,0 +1,15 @@
+from flask import url_for
+from application import db
+from application.models import Task
+from tests import TestBase
+
+
+class TestAdd(TestBase):
+  # adding a task via POST works
+  def test_add_task(self):
+    self.client.post(
+        url_for('add'),
+        data=dict(title='New task')
+    )
+    new_task = Task.query.first()
+    assert new_task.title == 'New task'
