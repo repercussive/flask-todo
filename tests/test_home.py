06fc13647684393d01298c7a3c962bbfd0eeaf87
= <liam2robertson@gmail.com>
2022-05-27 14:50:50 +0100

    rewrite tests using "assert"

--- tests/test_home.py
+++ tests/test_home.py
@@ -4,17 +4,17 @@ from application.models import Task
 from tests import TestBase
 
 
-class TestApp(TestBase):
+class TestHome(TestBase):
   # (home route, GET) gives a valid response on GET
-  def test_get_home(self):
+  def test_get(self):
     response = self.client.get(url_for('home'))
-    self.assert200(response)
+    assert response.status_code == 200
 
   # (home route, GET) the tasks in the db are sent in the response
-  def test_get_tasks(self):
+  def test_tasks_received(self):
     test_task = Task(title='A cool task')
     db.session.add(test_task)
     db.session.commit()
 
     response = self.client.get(url_for('home'))
-    self.assertIn(b'A cool task', response.data)
+    assert b'A cool task' in response.data
